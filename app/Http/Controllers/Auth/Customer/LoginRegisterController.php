58e2a7d7bd91a09eb2b8d2a98baa78eb7dd53fce
MyProject <104205952+gitSajjad@users.noreply.github.com>
2022-08-28 10:27:54 +0430

    Soap Mobile

--- app/Http/Controllers/Auth/Customer/LoginRegisterController.php
+++ app/Http/Controllers/Auth/Customer/LoginRegisterController.php
@@ -2,27 +2,30 @@
 
 namespace App\Http\Controllers\Auth\Customer;
 
+ 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Models\Auth\Customer\OTP;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Auth\Customer\StoreLoginRegisterRequest;
+use Illuminate\Support\Facades\Config;
+use App\Http\Services\Message\MessageSerivce;
+use App\Http\Services\Message\SMS\SmsService;
+use App\Http\Requests\Auth\Customer\LoginRegisterRequest;
 
 class LoginRegisterController extends Controller
 {
     public function loginRegisterForm()
     {
-      return view('customer.auth.login-register');
+        return view('customer.auth.login-register');
     }
 
-    public function LoginRegister(StoreLoginRegisterRequest $request)
+    public function loginRegister(LoginRegisterRequest $request)
     {
-
-
         $inputs = $request->all();
 
         //check id is email or not
         if(filter_var($inputs['id'], FILTER_VALIDATE_EMAIL))
-        
         {
             $type = 1; // 1 => email
             $user = User::where('email', $inputs['id'])->first();
@@ -32,7 +35,7 @@ class LoginRegisterController extends Controller
         }
 
         //check id is mobile or not
-        elseif(preg_match('/^(\+98|98|0)9\d{9}$', $inputs['id'])){
+        elseif(preg_match('/^(\+98|98|0)9\d{9}$/', $inputs['id'])){
             $type = 0; // 0 => mobile;
 
 
@@ -42,6 +45,7 @@ class LoginRegisterController extends Controller
             $inputs['id'] = str_replace('+98', '', $inputs['id']);
 
             $user = User::where('mobile', $inputs['id'])->first();
+            
             if(empty($user)){
                 $newUser['mobile'] = $inputs['id'];
             }
@@ -51,9 +55,55 @@ class LoginRegisterController extends Controller
             $errorText = 'شناسه ورودی شما نه شماره موبایل است نه ایمیل';
             return redirect()->route('auth.customer.login-register-form')->withErrors(['id' => $errorText]);
         }
+        
+        if(empty($user)){
+            $newUser['password'] = '98355154';
+            $newUser['activation'] = 1;
+            $user = User::create($newUser);
+        }
+
+        //create otp code
+        $otpCode = rand(111111, 999999);
+        $token = Str::random(60);
+        $otpInputs = [
+            'token' => $token,
+            'user_id' => $user->id,
+            'otp_code' => $otpCode,
+            'login_id' => $inputs['id'],
+            'type' => $type,
+        ];
+
+        OTP::create($otpInputs);
+
+        //send sms or email
+
+        if($type == 0){
+            //send sms
+            $smsService = new SmsService();
+            $smsService->setFrom(Config::get('sms.otp_from'));
+            $smsService->setTo(['0' . $user->mobile]);
+            $smsService->setText(" مجموعه انگیزشی دکتر ناصر خالدی \n  کد تایید : $otpCode");
+            $smsService->setIsFlash(true);
+
+            $messagesService = new MessageSerivce($smsService);
+
+        }
+
+        // elseif($type === 1){
+
+        // }
+
+        $messagesService->send();
+
+
 
 
     }
+
+
+
+
+
 
 
 }
--- app/Models/Auth/Customer/OTP.php
+++ app/Models/Auth/Customer/OTP.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class OTP extends Model
 {
     use HasFactory;
+
+
+    protected $guarded = ['id'];
+    protected $table = 'otps';
 }
